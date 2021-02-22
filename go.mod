cfa7a6b1dd4d5b389ade6940778b047a7b684467
Charlie Gillespie <usacfg@gmail.com>
2021-02-22 04:21:14 -0500

    made progress

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module charliegillespie.com/instapod
 
 go 1.15
+
+require (
+	github.com/google/uuid v1.2.0
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43
+)
--- main.go
+++ main.go
@@ -1,15 +1,90 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/sys/unix"
 )
 
-func main() {
+var podDirectory string
 
-	fmt.Print("Checking dependencies... ")
-	defer fmt.Print("done \n")
+type episode struct {
+	title       string
+	description string
+	uuid        uuid.UUID
+	addDate     time.Time
+	pubDate     time.Time
+}
+
+func main() {
+	checkDeps()
+	podDir := getDir()
+	uuidbin, _ := uuid.New().MarshalBinary()
+	uuidstring := hex.EncodeToString(uuidbin)
+
+	fullpath := filepath.Join(podDir, uuidstring)
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please supply a URL as an argument")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+	getAndConvert(url, fullpath)
+}
+
+func getDir() string {
+	usr, _ := user.Current()
+	homedir := usr.HomeDir
+	podDir := filepath.Join(homedir, ".instapod")
+
+	_, err := os.Stat(podDir)
+
+	if os.IsNotExist(err) {
+		err := os.Mkdir(podDir, 0755)
+		if err != nil {
+			fmt.Println("Could not create ~/.instapod folder. Check your permissions.")
+		}
+	} else if err != nil {
+		panic(err)
+	}
+
+	if unix.Access(podDir, unix.W_OK) != nil {
+		fmt.Println("Could not read ~/.instapod folder. Check your permissions.")
+		os.Exit(1)
+	}
+
+	return podDir
+}
+
+func getAndConvert(videoURL string, name string) error {
+	command := exec.Command("youtube-dl", "-x", "--audio-format", "mp3",
+		"--audio-quality", "64k", "--embed-thumbnail",
+		"--add-metadata", "--print-json",
+		"--postprocessor-args", "-ac 1",
+		"-o", name+".%(ext)s",
+		videoURL)
+	output, err := command.Output()
+
+	fmt.Print(string(output))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func checkDeps() {
+	// fmt.Print("Checking dependencies... ")
+	// defer fmt.Print("done \n")
 	_, err := exec.Command("youtube-dl", "--version").Output()
 
 	if err != nil {
@@ -24,11 +99,4 @@ func main() {
 		fmt.Println("Instapod requires FFmpeg (https://ffmpeg.org)")
 		os.Exit(1)
 	}
-
-	outp, err := exec.Command("youtube-dl", "-x", "--audio-format mp3",
-		"--audio-quality 64k", "https://www.youtube.com/watch?v=SQ8UDzUvMg0").Output()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Print(string(outp))
 }
